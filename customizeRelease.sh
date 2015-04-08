b8ed8fad708cb65404cc5c21646055208ad621ee
Sabha Parameswaran <sparameswaran@gopivotal.com>
2015-04-08 10:17:39 -0700

    Add app_version as part of the Tile

--- customizeRelease.sh
+++ customizeRelease.sh
@@ -70,7 +70,8 @@ if [ "${requireEnvVariables:0:1}" == "y" ]; then
     #echo "                  s#${templated_variableName}#\${${variableName}}#g;  \\ " >> $erbTmp2
 
     if [ "${exposable:0:1}" == "y" ]; then
-      echo "      - reference: .properties.${variableName}"  >> $tileTmp1
+#      echo "      - reference: .properties.${variableName}"  >> $tileTmp1
+      echo "      - reference: .${variableName}"  >> $tileTmp1
       echo "        label: ${variableLabel}  "  >> $tileTmp1
       echo "        description: ${variableDescrp}  "  >> $tileTmp1
     fi
