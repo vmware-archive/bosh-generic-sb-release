69b7999f1da84288c44d3cee8a0cd3aec9668a07
Sabha Parameswaran <sparameswaran@gopivotal.com>
2015-11-10 10:44:58 -0800

    Check for app health before exiting from deploy broker errand

--- createTile.sh
+++ createTile.sh
@@ -5,15 +5,14 @@ TILE_NAME=Generic-Broker-Experimental
 TILE_FILE=`pwd`/*tile-v${TILE_VERSION}.yml
 RELEASE_TARFILE=`pwd`/releases/*/*.tgz
 
-#BOSH_STEMCELL_FILE=`cat ${TILE_FILE} | grep "bosh-stemcell" | grep "^ *file:" | awk '{print $2}' `
-#BOSH_STEMCELL_LOCATION=https://s3.amazonaws.com/bosh-jenkins-artifacts/bosh-stemcell/vsphere
-
+rm -rf tmp
 mkdir -p tmp
 pushd tmp
 #Dont bundle the stemcell into the .pivotal Tile file as the stemcell must already be available in the Ops Mgr.
 mkdir -p metadata releases #stemcells
 cp $TILE_FILE metadata
 cp $RELEASE_TARFILE releases
+cp -r ../content_migrations .
 
 # Ignore bundling the stemcell as most often the Ops Mgr carries the stemcell.
 # If Ops Mgr complains of missing stemcell, change the version specified inside the tile to the one that Ops mgr knows about
@@ -21,6 +20,6 @@ cp $RELEASE_TARFILE releases
 #if [ ! -e "stemcells/$BOSH_STEMCELL_FILE" ]; then
 #  curl -k $BOSH_STEMCELL_LOCATION/$BOSH_STEMCELL_FILE -o stemcells/$BOSH_STEMCELL_FILE
 #fi
-zip -r ${TILE_NAME}-${TILE_VERSION}.pivotal metadata releases #stemcells
+zip -r ${TILE_NAME}-${TILE_VERSION}.pivotal metadata releases content_migrations
 mv ${TILE_NAME}-${TILE_VERSION}.pivotal ..
 popd
